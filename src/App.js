86c9b48ac01bd89af101a3d5c7d06394d6fbb51b
Jerrydev01 <francisjerry360@gmail.com>
2022-05-19 11:00:39 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from "react";
 import "./style.css";
 
-export default function App() {
+export default function App({title,children}) {
+  const lists=[
+  {
+    name: 'jerry',
+    age: 23,
+    lastName: 'francis'
+  },
+{
+  name:'peace',
+  age: 32,
+  lastName: 'franky'
+}
+]
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Hello Stack</h1>
+      <p></p>
+      {lists.map((list)=>{
+        console.log(list)
+        return <h1>{list.lastName}</h1>
+      })}
+    </div>
+  );
+}
+
+
+
+export default function Props() {
+  return (
+    <div>
+    <App title='james'>
+      <p>loremergjkkfcchjj</p>
+    </App>
     </div>
   );
 }
