4b5407c8f6e496575f95da88065a8eee9a395a0d
Jerrydev01 <francisjerry360@gmail.com>
2022-05-27 04:37:14 +0100

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
-export default function App({title,children}) {
-  const lists=[
-  {
-    name: 'jerry',
-    age: 23,
-    lastName: 'francis'
-  },
-{
-  name:'peace',
-  age: 32,
-  lastName: 'franky'
-}
-]
+import { data } from './Data';
+
+export default function App() {
+  // console.log(useState('hello'))
+  const [title, setTitle] = useState(data);
 
   return (
-    <div>
-      <h1>Hello Stack</h1>
-      <p></p>
-      {lists.map((list)=>{
-        console.log(list)
-        return <h1>{list.lastName}</h1>
+    <>
+      {title.map((person) => {
+      return  <div key={person.id}>
+         <h2>{person.name}</h2>
+        </div>;
       })}
-    </div>
-  );
-}
-
-
-
-export default function Props() {
-  return (
-    <div>
-    <App title='james'>
-      <p>loremergjkkfcchjj</p>
-    </App>
-    </div>
+    </>
   );
 }
---
+++ src/Data.jsx
@@ -0,0 +1,10 @@
+export const data = [
+  {
+    id: 1,
+    name: 'jerry',
+  },
+  {
+    id:2,
+    name:'peace'
+  }
+];
