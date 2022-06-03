613b21ecada92eae377115f20cfae5c57fd869cc
Jerrydev01 <francisjerry360@gmail.com>
2022-06-03 14:06:52 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
-import { data } from './Data';
-
 export default function App() {
-  // console.log(useState('hello'))
-  const [size, setSize] = useState(window.innerWidth);
-console.log(size)
-const sizeChange =()=>{
-  setSize(window.innerWidth)
-  return ()=>{
-    setSize(window.innerWidth)
-  }
-}
-useEffect(()=>{
-window.addEventListener('resize', sizeChange)
-});
+  const [text, setText] = useState('');
+  const firstValue = text || 'hello world';
+  const secondValue = text && 'hello world';
+
   return (
     <>
-<h1>UseEffect cleanup</h1>
-  <h2>{size}px</h2>
+      <h1>{text || 'hello world'}</h1>
+      <h1>{!text && <h1>hello world</h1>}</h1>
     </>
   );
 }
