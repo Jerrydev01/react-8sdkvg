0bb55e3a23e2d1f2608839c6bd70ed3b7915471e
Jerrydev01 <francisjerry360@gmail.com>
2022-06-03 15:29:48 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React, { useState, useEffect } from 'react';
 import './style.css';
 
 export default function App() {
-  const [text, setText] = useState('');
-  const firstValue = text || 'hello world';
-  const secondValue = text && 'hello world';
+  const [isShow, setIsShow] = useState(false);
 
   return (
     <>
-      <h1>{text || 'hello world'}</h1>
-      <h1>{!text && <h1>hello world</h1>}</h1>
+      <h2>SHOW/HIDE</h2>
     </>
   );
 }
