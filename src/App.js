8764d9b8aa8eec888bfef31e096cc028fcc18639
Jerrydev01 <francisjerry360@gmail.com>
2022-06-04 21:56:38 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,10 +4,21 @@ import './style.css';
 const App = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello world');
+    if (name && email) {
+      const person = { id:new Date().getTime().toString(), name, email };
+      setPeople((people) => {
+        return [...people, person];
+      });
+
+      setName('');
+      setEmail('');
+    } else {
+      console.log('world');
+    }
   };
   return (
     <>
@@ -15,22 +26,32 @@ const App = () => {
       <div className="form" onSubmit={handleSubmit}>
         <form>
           <label htmlFor="name">Name:</label>
-          <input 
-          type="text" 
-          id="name" 
-          value={name}
-          onChange={(e)=>setName(e.target.value)}
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
           <br />
           <label htmlFor="email">Email:</label>
-          <input 
-          type="email" 
-          id="email" 
-          value={email}
-          onChange={(e)=>setEmail(e.target.value)}
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <button type="submit">Submit</button>
         </form>
+
+        {people.map((person) => {
+          const {id, name, email} = person;
+          return (
+            <div key={id}>
+              <h4>{name}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </div>
     </>
   );
