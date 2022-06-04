2727c4adea3fd9fad895f85e6490fad84a6beff4
Jerrydev01 <francisjerry360@gmail.com>
2022-06-04 02:06:09 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
 
-export default function App() {
-  const [isShow, setIsShow] = useState(false);
+const App = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello world');
+  };
   return (
     <>
-      <h2>SHOW/HIDE</h2>
+      <h2>FORMS</h2>
+      <div className="form" onSubmit={handleSubmit}>
+        <form>
+          <label htmlFor="name">Name:</label>
+          <input 
+          type="text" 
+          id="name" 
+          value={name}
+          onChange={(e)=>setName(e.target.value)}
+          />
+          <br />
+          <label htmlFor="email">Email:</label>
+          <input 
+          type="email" 
+          id="email" 
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </>
   );
-}
+};
+
+export default App;
